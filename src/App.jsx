9e22927b2db6ca431ee450fc05603b3cea7b69ae
Bishal-laha <bishallaha12345@gmail.com>
2024-07-21 23:40:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Circle from './component/Circle';
 
 const App = () => {
   return (
-    <div className='justify-center items-center flex w-full bg-blue-950'>
+    <div className='justify-center items-center flex w-full flex-col'>
+      <h1 className='text-center font-semibold mt-8 text-[1.7rem]'>Documents Required for Study Abroad Loan</h1>
       <div className='mt-[8%] mb-[18%]'>
         <Circle />
       </div>
